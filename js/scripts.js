11a099684e91190a14ce81568f47d48e965446d1
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-09-21 16:39:31 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const info = document.getElementById("feched-data");
 getInfo();
 
+//Help from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+//And discussion with Viia Mäntymäki
 async function getInfo() {
   const url =
     "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
@@ -27,3 +29,10 @@ async function getInfo() {
     }
   });
 }
+
+async function getEmpInfo(){
+  const newUrl = "https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065";
+  const newPromis = await fetch(newUrl);
+  let EmpData = await newPromis.json();
+}
+
